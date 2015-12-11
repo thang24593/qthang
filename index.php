a3a86388ae56943df067463fc944c9a9db09f4e8
thang24593 <thang24593@gmail.com>
2015-12-11 14:04:18 +0700

    update index - 2

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <!-- Meta charset -->
     <meta charset="UTF-8">
- 
+
     <!-- Viewport specification for mobile devices -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
